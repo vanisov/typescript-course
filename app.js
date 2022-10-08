7c17209e5fcde4db30222befbb9d1caa6fa26f58
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2022-10-08 00:05:02 -0700

    progress through course

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Vlad',
-    age: 18,
-    hobbies: ['Sports', 'Cooking']
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
-var favoriteActivities;
-favoriteActivities = ['Sports',];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+;
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
+;
--- app.ts
+++ app.ts
@@ -1,24 +1,22 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; // <--- THIS IS A TUPLE
-  // A tuple is a fixed-length array, and also a fixed-type array.
-} = {
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
+const person = {
   name: 'Vlad',
-  age: 18,
+  age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
-
-person.role.push('admin');
-person.role[1] = 10;
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports',];
 
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
-}
+};
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
+};
