d7d76c11629658b2e61e6a8233e6d7ec961130b4
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2022-11-27 20:54:16 -0800

    set up global project compiler with tsc

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var userInput; // unknown is the better option in comparison to 'any' because it makes sure you are not allowed to do anything, it provides some limitations.
-var userName;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-; // this is how you would define userInput into string.
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An error occurred!', 500);
+"use strict";
+const userName = 'Vlad Anisov';
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,14 +1,3 @@
-let userInput: unknown; // unknown is the better option in comparison to 'any' because it makes sure you are not allowed to do anything, it provides some limitations.
+const userName = 'Vlad Anisov';
 
-let userName: string;
-
-userInput = 'Max';
-if (typeof userInput === 'string') {
-  userName = userInput;
-}; // this is how you would define userInput into string.
-
-function generateError(message: string, code: number) {
-  throw { message: message, errorCode: code };
-}
-
-generateError('An error occurred!', 500);
+console.log(userName);
