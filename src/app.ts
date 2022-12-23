dce46bf2d5f856b00a283895846bd73f7b98c03c
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2022-12-23 12:41:03 -0800

    continuing through typescript course

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
 const userName = 'Vlad';
+
+const add = (a: number, b: number = 0) => a + b;
+
+const printOutput: (a: string | number) => void = output => console.log(output);
+
+const button = document.querySelector('button')
+
+if (button){
+  button.addEventListener('click', event => console.log(event));
+}
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: 'Vlad',
+  age: 18
+};
+
+const copiedPerson = { ...person }
